4f8ae08f59cb1f956c4e1bbd0a025d6535a58b90
Otávio Prado <otavio_lipe@hotmail.com>
2015-07-29 16:24:20 -0300

    Teste funcional automático da tela de login

--- functest/src/test/java/br/com/milenio/vendingmachine/functest/login/LoginTest.java
+++ functest/src/test/java/br/com/milenio/vendingmachine/functest/login/LoginTest.java
@@ -1,32 +1,103 @@
 package br.com.milenio.vendingmachine.functest.login;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
 
+@Test
 public class LoginTest {
+	
+	private static final String BTN_LOGIN = "btnLogin";
+	private static final String SENHA = "senha";
+	private static final String USUARIO = "usuario";
+	private static final String HTTP_LOCALHOST_8080_VENDINGMACHINE = "http://localhost:8080/vendingmachine";
+	private WebElement usuario;
+	private WebElement senha;
+	private WebElement btnLogin;
+	private WebDriver driver;
+	private WebElement messages;
+	
 	@Test
-	public void testDeveriaRealizarLoginComSucessoAtravesDeUsuarioSenhaValidos() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://localhost:8080/vendingmachine");
-
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("usuario"));
-        element.sendKeys("otavio");
-        element = driver.findElement(By.name("senha"));
-        element.sendKeys("gudiao");
-        element = driver.findElement(By.name("login"));
-        
-        element = driver.findElement(By.name("btn-login"));
-        element.click();
+	public void deveriaRealizarLoginComSucessoAtravesDeUsuarioSenhaValidos() {
+		driver = new FirefoxDriver();
+		driver.get(HTTP_LOCALHOST_8080_VENDINGMACHINE);
+		
+		usuario = driver.findElement(By.name(USUARIO));
+		senha = driver.findElement(By.name(SENHA));
+		btnLogin = driver.findElement(By.name(BTN_LOGIN));
+		
+		usuario.sendKeys("otavio");
+		senha.sendKeys("gudiao");
+        btnLogin.click();
         
         // Check the title of the page
-        Assert.assertEquals("Sistema de Gest√£o de Vending Machines", driver.getTitle());
+        AssertJUnit.assertEquals("Sistema de Gest„o de Vending Machines", driver.getTitle());
         
-        //Close the browser
+        driver.quit();
+    }
+	
+	@Test
+	public void deveriaValidarUsuarioInexistente() {
+		driver = new FirefoxDriver();
+		driver.get(HTTP_LOCALHOST_8080_VENDINGMACHINE);
+		
+		usuario = driver.findElement(By.name(USUARIO));
+		senha = driver.findElement(By.name(SENHA));
+		btnLogin = driver.findElement(By.name(BTN_LOGIN));
+		
+		usuario.sendKeys("ZEH");
+		senha.sendKeys("gudiao");
+        btnLogin.click();
+        messages = driver.findElement(By.id("messages"));
+        String mensagem = messages.getText();
+        
+        Assert.assertEquals(mensagem, "Usu·rio ZEH n„o existe cadastrado no sistema.");
+        
+        driver.quit();
+    }
+	
+	@Test
+	public void deveriaValidarUsuarioBloqueado() {
+		driver = new FirefoxDriver();
+		driver.get(HTTP_LOCALHOST_8080_VENDINGMACHINE);
+		
+		usuario = driver.findElement(By.name(USUARIO));
+		senha = driver.findElement(By.name(SENHA));
+		btnLogin = driver.findElement(By.name(BTN_LOGIN));
+		
+		usuario.sendKeys("bruna");
+		senha.sendKeys("gudiao");
+        btnLogin.click();
+        messages = driver.findElement(By.id("messages"));
+        String mensagem = messages.getText();
+        
+        Assert.assertEquals(mensagem, "Este usu·rio est· bloqueado no sistema. Motivo: Afastamento");
+        
+        driver.quit();
+    }
+	
+	@Test
+	public void deveriaValidarUsuarioSemPermissaoDeLogin() {
+		driver = new FirefoxDriver();
+		driver.get(HTTP_LOCALHOST_8080_VENDINGMACHINE);
+		
+		usuario = driver.findElement(By.name(USUARIO));
+		senha = driver.findElement(By.name(SENHA));
+		btnLogin = driver.findElement(By.name(BTN_LOGIN));
+		
+		usuario.sendKeys("pedro");
+		senha.sendKeys("gudiao");
+        btnLogin.click();
+        String mensagem;
+    	messages = driver.findElement(By.id("accessDenied"));
+    	mensagem = messages.getText();
+
+        Assert.assertEquals(mensagem, "Erro: Acesso negado");
+        
         driver.quit();
     }
 }
